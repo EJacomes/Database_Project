34e0f93de995bed3f7c819198eb4deb39a793f3d
Erick Jacomes <137849397+EJacomes@users.noreply.github.com>
2025-05-01 14:30:00 -0400

    Add files via upload

---
+++ DatabaseProject/ConnectionFigure.php
@@ -0,0 +1,8 @@
+<?php
+$serverName = "localhost"
+$userName = "";
+$password = "";  // root :avi ;
+$dbName = "FigureDatabase";
+
+$conn = mysqli_connect($serverName, $userName, $password, $dbName);
+?>
---
+++ DatabaseProject/Homepage.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <style>
+            body {
+                background-color: black;
+                color:white;
+                font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+            }
+
+            .button {
+                 background-color: gray;
+                 color: white;
+                 padding: 15px 32px;
+                 text-align: center;
+                 text-decoration: none;
+                 display: inline-block;
+                 font-size: 16px;
+                 margin: 4px 2px;
+                 cursor: pointer;
+                }
+        </style>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
+        <title>HomePage</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
